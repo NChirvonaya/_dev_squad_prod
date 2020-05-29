49819f4285067e8e50f1fe1ba0932d4c04681b3b
Nastya Chirvonaya <nastyachirvonaya@gmail.com>
2020-05-29 14:40:43 +0300

    Updated templates

--- instagrammer.py
+++ instagrammer.py
@@ -1,3 +1,12 @@
+"""Starts app."""
+from flask import render_template
 from app import create_app
 
 server = create_app()
+
+
+@server.errorhandler(404)
+def page_not_found(error):
+    """404 page."""
+    # note that we set the 404 status explicitly
+    return render_template("404.html"), 404
